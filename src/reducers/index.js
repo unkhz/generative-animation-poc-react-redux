46447903b283ee6201cf45c0c169766c1c20967c
Juhani Pelli <juhani.pelli@gmail.com>
2016-04-17 18:20:16 +0100

    Use lodash for reducer chaining

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,35 @@
 // @flow
 import { combineReducers } from 'redux';
-import PipeReducer from 'utils/PipeReducer';
+import flow from 'lodash/fp/flow';
+import thru from 'lodash/fp/thru';
 import {ParticleType, ActionType, GlobalStateType} from 'constants/Types';
 import {environment} from './environment';
 import {layers} from './layers';
 import {particles} from './particles';
 
+export default (state: GlobalStateType, action: ActionType): GlobalStateType => {
+  return flow(
+    pipe(environment, action),
+    pipe(layers, action),
+    pipe(particles, action),
+    pipe(countParticles, action),
+  )(state);
+};
+
+/**
+ * Create a pipeable version of a reducer function with a bound action
+ */
+function pipe(reducer: Function, action: ActionType): Function {
+  return ((reducer: Function, action: ActionType, state: GlobalStateType): GlobalStateType => {
+    return {
+      ...state,
+      ...reducer(state, action)
+    };
+  }).bind(null, reducer, action);
+}
 
 function countParticles(state: GlobalStateType): GlobalStateType {
   return {
     aliveParticleCount: state.particles.filter((p: ParticleType) => !p.isToBeDestroyed).length
   };
 }
-
-export default (state: GlobalStateType, action: ActionType): GlobalStateType => {
-  return new PipeReducer(state, action)
-    .bind(environment)
-    .bind(layers)
-    .bind(particles)
-    .bind(countParticles)
-    .end();
-};
