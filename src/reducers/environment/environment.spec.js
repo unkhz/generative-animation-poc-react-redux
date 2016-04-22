8b2aae8392a75ea11aa978348b36831b4e0c40e7
Juhani Pelli <juhani.pelli@gmail.com>
2016-04-22 18:44:43 +0100

    Improve environment unit tests

--- src/reducers/environment/environment.spec.js
+++ src/reducers/environment/environment.spec.js
@@ -1,4 +1,4 @@
-import * as actionTypes from 'constants/ActionTypes';
+import * as actions from 'actions/Actions';
 import {environment} from 'reducers/environment';
 import {assert} from 'chai';
 
@@ -10,21 +10,17 @@ describe('environment reducer', () => {
   });
 
   it('produces radius on envResized on portrait', () => {
-    const state = environment({}, {
-      type: actionTypes.ENV_RESIZED,
-      width: 768,
-      height: 1024
-    });
+    const state = environment({}, actions.envResized(768, 1024));
     assert.equal(state.env.radius, 768/2);
+    assert.equal(state.env.width, 768);
+    assert.equal(state.env.height, 1024);
   });
 
   it('produces radius on envResized on landscape', () => {
-    const state = environment({}, {
-      type: actionTypes.ENV_RESIZED,
-      width: 14425,
-      height: 142
-    });
+    const state = environment({}, actions.envResized(14425, 142));
     assert.equal(state.env.radius, 142/2);
+    assert.equal(state.env.width, 14425);
+    assert.equal(state.env.height, 142);
   });
 
 });
